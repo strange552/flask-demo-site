77b5b346fff135558c3a3c3233dba58556776716
Dr Strange <drstrangestrange902@gmail.com>
2025-05-16 10:12:28 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import logging
+import os
+
+app = Flask(__name__)
+
+# Logging setup
+logging.basicConfig(filename='access.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+@app.route('/')
+def home():
+    app.logger.info("Home page visited from %s", request.remote_addr)
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    app.logger.info("About page visited from %s", request.remote_addr)
+    return render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
---
+++ static/js/script.js
@@ -0,0 +1,3 @@
+function sayHello() {
+  alert("Hello from your colorful Flask site! 🌈");
+}
